092c9e3bb251ac0d40a1f3b9c6fb1aa67ff59c57
yiir27 <camp.yui.c@gmail.com>
2023-10-12 01:56:59 +0900

    Model作成

--- app/Models/AuthorDetail.php
+++ app/Models/AuthorDetail.php
@@ -5,10 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
+class AuthorDetail extends Model
 {
     use HasFactory;
-
-    //タイトルの列の登録を許可する
-    protected $fillable = ['title'];
 }
