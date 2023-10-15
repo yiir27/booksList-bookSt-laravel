46fec9ac4021e7f1832d35a2762a68a69f782eaf
yiir27 <camp.yui.c@gmail.com>
2023-10-16 01:29:00 +0900

    reration終了

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
@@ -11,4 +12,9 @@ class Category extends Model
 
     //タイトルの列の登録を許可する
     protected $fillable = ['title'];
+
+    public function books(): HasMany
+    {
+        return $this->hasMany(Book::class);
+    }
 }
