9484c77caca6347b3635029d79f7405f334f82bd
yiir27 <camp.yui.c@gmail.com>
2023-10-16 11:02:46 +0900

    Controller完

--- app/Http/Controllers/Admin/BookController.php
+++ app/Http/Controllers/Admin/BookController.php
@@ -1,8 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
+use App\Http\Controllers\Controller;
 use App\Models\Book;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
@@ -10,9 +12,13 @@ class BookController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index():Collection
     {
-        //
+        //書籍一覧を取得
+        $books = Book::all();
+
+        //書籍一覧をレスポンスとして返す
+        return $books;
     }
 
     /**
@@ -34,9 +40,13 @@ class BookController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Book $book)
+    public function show(string $id):Book
     {
-        //
+        //書籍一件を取得
+        $book = Book::findOrFail($id);
+
+        //取得した書籍をレスポンスとして返す
+        return $book;
     }
 
     /**
